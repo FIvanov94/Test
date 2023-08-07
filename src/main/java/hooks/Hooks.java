42a2ad0e52e8eaddced61e83a6bdcd9915e929a7
FIvanov94 <filip.tihomirov.ivanov@gmail.com>
2023-08-07 11:55:27 +0300

    test

--- src/main/java/hooks/Hooks.java
+++ src/main/java/hooks/Hooks.java
@@ -16,7 +16,7 @@ import java.time.Duration;
 public class Hooks {
 
     //   Edit the directory of the application according to your installation directory
-    private static final String MUSEEKS = "E:\\Projects\\src\\main\\resources\\Program\\museeks\\Museeks.exe";
+    private static final String MUSEEKS = "C:\\Users\\filip\\AppData\\Local\\Programs\\museeks\\Museeks.exe";
 
     DriverManager driverManager = new DriverManager();
 
